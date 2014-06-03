ffeb04391ec69a4e5a4e96ece91de43a68aa360d
alex <alexburkhardt@gmx.de>
2014-06-03 13:32:14 +0200

    Run mode geht hacky

--- ExecutionWorker.java
+++ ExecutionWorker.java
@@ -1,21 +1,28 @@
 import javax.swing.SwingWorker;
+import  java.util.concurrent.locks.*;
 
 class ExecutionWorker extends SwingWorker<Integer, Integer> {
   
   private Steuerung steuerung;
+  private MainFrame mainFrame;
   
-  public ExecutionWorker(Steuerung aSteuerung) {
+  public ExecutionWorker(Steuerung aSteuerung, MainFrame aMainFrame) {
     
     super();
     steuerung = aSteuerung;
+    mainFrame = aMainFrame;
     
   }
   
   protected Integer doInBackground() throws Exception {
     
     while (steuerung.getRunning()) { 
+      
       steuerung.executeOneCommand();
-      steuerung.getMainFrame().automaticTableScroll();
+      mainFrame.updateElements();
+      mainFrame.automaticTableScroll();
+      
+      
     }
     
     return 0;
@@ -25,6 +32,8 @@ class ExecutionWorker extends SwingWorker<Integer, Integer> {
   protected void done() {
     
     
-    
   }
+  
+  
+  
 }
